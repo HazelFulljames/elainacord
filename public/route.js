23fbbedc06fe4ca373f1ccd3ca29441e17828466
Hazel <fulljameshazel@gmail.com>
2025-05-24 01:08:22 -0700

    work please

--- public/route.js
+++ public/route.js
@@ -1,15 +1,15 @@
 import { Router, urlencoded } from "express";
 import "dotenv/config";
-import MongoStore from "connect-mongo";
+// import MongoStore from "connect-mongo";
 const mongodb_database = process.env.MONGODB_DATABASE;
 var {database} = include('databaseConnection');
-const collection = database.db(mongodb_database).collection('messages');
+const msgcollection = database.db(mongodb_database).collection('messages');
 const router = Router();
 
 router.post('/addMessage', async (req, res) => {
     const { userinp, messageinp } = req.body;
     console.log(req.body)
-    await collection.insertOne({
+    await msgcollection.insertOne({
         user: userinp,
         message: messageinp
     });
@@ -17,7 +17,7 @@ router.post('/addMessage', async (req, res) => {
 });
 
 router.get('/getMessages', async (req, res) => {
-  let messages = await collection.find().toArray();
+  let messages = await msgcollection.find().toArray();
 
   return res.status(200).json({messages: messages});
 });
